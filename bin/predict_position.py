84f440758e8b8dd27f764998ce143ebbcbc5307f
samuel <samuel@samuel>
2021-02-16 22:38:03 -0500

    reformatted for runPuckVis

--- bin/predict_position.py
+++ bin/predict_position.py
@@ -13,16 +13,15 @@ from tensorflow.keras.models import load_model
 from datetime import date
 
 model_dir = '/home/samuel/Desktop/Classes/CSC_300/puck_visualization_system/models/'
-model_name = 'puck_visualization_model_' + date.today().strftime('%b_%d_%Y') + '.h5'
+model_name = 'puck_visualization_model_13Feb21.h5'
 
 tf.get_logger().setLevel('ERROR')
 new_model = load_model(model_dir + model_name)
 
-category_names = ['Empty', 'Straight', 'Tilted']
-IMG_HEIGHT = 150
-IMG_WIDTH = 150
 
 def prepare_image(img_name):
+    IMG_HEIGHT = 150
+    IMG_WIDTH = 150
     input_image = cv2.imread(img_name)
     image_resize = cv2.resize(
         input_image, (IMG_HEIGHT, IMG_WIDTH))
@@ -33,16 +32,17 @@ def prepare_image(img_name):
 
 
 def predict_image(img):
+    category_names = ['Empty', 'Straight', 'Tilted']
     prediction = np.argmax(new_model.predict(
         [prepare_image(img)]), axis=-1)
     print(category_names[prediction[0]])
 
 
-for i in range(1, 17):
-    fbase = sys.argv[3].split('.')[0]
-    img = sys.argv[2] + '/' + fbase + '_' + str(i) + '.jpg'
-    print('Postion: ' + str(i))
-    predict_image(img)
-    # p1 = multiprocessing.Process(target=predict_image(img))
-    # p1.start()
-# print(p1)
+# for i in range(1, 17):
+#     fbase = sys.argv[3].split('.')[0]
+#     img = sys.argv[2] + '/' + fbase + '_' + str(i) + '.jpg'
+#     print('Postion: ' + str(i))
+#     predict_image(img)
+#     # p1 = multiprocessing.Process(target=predict_image(img))
+#     # p1.start()
+# # print(p1)
